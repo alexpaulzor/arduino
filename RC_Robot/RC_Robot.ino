1c92b4fb981a3377d1aa26677cc804715f24b8d7
Alex Paul <alex@alexpaul.net>
2018-12-29 16:36:42 -0800

    WIP: Progress

--- RC_Robot/RC_Robot.ino
+++ RC_Robot/RC_Robot.ino
@@ -1,57 +1,65 @@
 #include <PPMReader.h>
-
-// #define DRIVE_STEPPER
-#define DRIVE_PWM
+#include <AccelStepper.h>
 
 #define ENABLE_SERIAL 0
 #define IF_SERIAL if (ENABLE_SERIAL)
 
+#define ARM_MICROSTEP 1
+#undef ARM_L298N
+
 #define DRIVE_MS 100
 #define RC_TIMEOUT_S 3
 #define THROTTLE_WEIGHT 100
-#define STEERING_WEIGHT 50
+#define STEERING_WEIGHT 100
 #define PPM_LOW 1000
 #define PPM_HIGH 2000
 #define PPM_CENTER ((PPM_LOW + PPM_HIGH) / 2)
 
-#define STATUS_LED_PIN 2
+#define STATUS_LED_PIN 13
 #define INTERRUPT_PIN 3
 #define RC_POWER_PIN 12
 
-#ifdef DRIVE_STEPPER
-  #include <AccelStepper.h>
-  #define STEPPER_STEPS 800
-  #define MAX_SPEED_STEPS_PER_SEC 1600
-  #define L_STEPPER_PIN_I1 4
-  #define L_STEPPER_PIN_I2 5
-  #define L_STEPPER_PIN_I3 6
-  #define L_STEPPER_PIN_I4 7
-  #define R_STEPPER_PIN_I1 8
-  #define R_STEPPER_PIN_I2 9
-  #define R_STEPPER_PIN_I3 10
-  #define R_STEPPER_PIN_I4 11
-  
-  AccelStepper left_stepper(AccelStepper::FULL4WIRE,
-      L_STEPPER_PIN_I1, L_STEPPER_PIN_I2, L_STEPPER_PIN_I3, L_STEPPER_PIN_I4);
-  AccelStepper right_stepper(AccelStepper::FULL4WIRE,
-      R_STEPPER_PIN_I1, R_STEPPER_PIN_I2, R_STEPPER_PIN_I3, R_STEPPER_PIN_I4);
+#ifdef ARM_L298N
+  #define STEPPER_STEPS_PER_REV 200
+  #define MAX_SPEED_STEPS_PER_SEC 400
+  #define H_STEPPER_PIN_I1 26
+  #define H_STEPPER_PIN_I2 27
+  #define H_STEPPER_PIN_I3 28
+  #define H_STEPPER_PIN_I4 29
+  AccelStepper height_stepper(AccelStepper::FULL4WIRE,
+      H_STEPPER_PIN_I1, H_STEPPER_PIN_I2, H_STEPPER_PIN_I3, H_STEPPER_PIN_I4);
 #endif
 
-#ifdef DRIVE_PWM
-  #define PWM_MAX 255
-  #define L_FORWARD_PIN 4
-  #define L_FORWARD_SPEED_PIN 5
-  #define L_REVERSE_SPEED_PIN 6
-  #define L_REVERSE_PIN 7
-  #define R_FORWARD_PIN 8
-  #define R_REVERSE_PIN 9
-  #define R_FORWARD_SPEED_PIN 10
-  #define R_REVERSE_SPEED_PIN 11
+#ifdef ARM_MICROSTEP
+  #define STEPPER_STEPS_PER_REV 200
+  #define MAX_SPEED_STEPS_PER_SEC 400
+  #define H_STEPPER_PIN_ENABLE 51
+  #define H_STEPPER_PIN_DIR 52
+  #define H_STEPPER_PIN_PUL 53
+  AccelStepper height_stepper(AccelStepper::DRIVER,
+      H_STEPPER_PIN_PUL, H_STEPPER_PIN_DIR);
+  height_stepper.setEnablePin(H_STEPPER_PIN_ENABLE);
 #endif
+
+#define H_STEPPER_MAX_STEPS 20000  // TODO: measure
+#define H_STEPPER_HOME_SW_PIN 50
+#define H_STEPPER_HOME_THRESH (H_STEPPER_MAX_STEPS / 20.0)
+#define H_STEPPER_FUZZ (STEPPER_STEPS_PER_REV / 2)
+#define HEIGHT_WEIGHT H_STEPPER_MAX_STEPS
+  
+#define PWM_MAX 255
+#define L_FORWARD_PIN 4
+#define L_FORWARD_SPEED_PIN 5
+#define L_REVERSE_SPEED_PIN 6
+#define L_REVERSE_PIN 7
+#define R_FORWARD_PIN 8
+#define R_REVERSE_PIN 9
+#define R_FORWARD_SPEED_PIN 10
+#define R_REVERSE_SPEED_PIN 11
 
 #define AC_RELAY_CONTROL_PIN 13
 
-#define NUM_CHANNELS 6
+#define NUM_CHANNELS 7
 
 #define CHANNEL_R_EW 0
 #define CHANNEL_R_NS 1
@@ -59,11 +67,13 @@
 #define CHANNEL_L_EW 3
 #define CHANNEL_SWA 4
 #define CHANNEL_SWB 5
+#define CHANNEL_DIAL_VRA 6
 
 #define CHANNEL_THROTTLE CHANNEL_L_NS
 #define CHANNEL_STEERING CHANNEL_R_EW
 #define CHANNEL_AC_RELAY CHANNEL_SWB
 #define CHANNEL_ENABLE_DRIVE CHANNEL_SWA
+#define CHANNEL_HEIGHT CHANNEL_DIAL_VRA
 
 PPMReader ppm(INTERRUPT_PIN, NUM_CHANNELS);
 unsigned long last_ppm_signal = 0;
@@ -87,13 +97,10 @@ void setup() {
   digitalWrite(RC_POWER_PIN, LOW);
   blink(5, 200);
   digitalWrite(RC_POWER_PIN, HIGH);
-  #ifdef DRIVE_STEPPER
-  left_stepper.setMaxSpeed(MAX_SPEED_STEPS_PER_SEC);
-  right_stepper.setMaxSpeed(MAX_SPEED_STEPS_PER_SEC);
-  left_stepper.setSpeed(0);
-  right_stepper.setSpeed(0);
-  #endif
-  #ifdef DRIVE_PWM
+  height_stepper.setMaxSpeed(MAX_SPEED_STEPS_PER_SEC);
+  height_stepper.setSpeed(0);
+  height_stepper.setCurrentPosition(H_STEPPER_MAX_STEPS);
+ 
   pinMode(L_FORWARD_PIN, OUTPUT);
   pinMode(L_REVERSE_PIN, OUTPUT);
   pinMode(L_FORWARD_SPEED_PIN, OUTPUT);
@@ -102,7 +109,9 @@ void setup() {
   pinMode(R_REVERSE_PIN, OUTPUT);
   pinMode(R_FORWARD_SPEED_PIN, OUTPUT);
   pinMode(R_REVERSE_SPEED_PIN, OUTPUT);
-  #endif
+
+  pinMode(H_STEPPER_HOME_SW_PIN, INPUT);
+  
   stop_motors();
 }
 
@@ -138,7 +147,9 @@ void loop() {
         map(channel_values[CHANNEL_THROTTLE],
             PPM_LOW, PPM_HIGH, -THROTTLE_WEIGHT, THROTTLE_WEIGHT),
         map(channel_values[CHANNEL_STEERING],
-            PPM_LOW, PPM_HIGH, -STEERING_WEIGHT, STEERING_WEIGHT));
+            PPM_LOW, PPM_HIGH, -STEERING_WEIGHT, STEERING_WEIGHT),
+        map(channel_values[CHANNEL_HEIGHT],
+            PPM_LOW, PPM_HIGH, 0, HEIGHT_WEIGHT));
    } else {
     stop_motors();
     delay(DRIVE_MS);
@@ -146,15 +157,9 @@ void loop() {
   }
 }
 
-void drive(int throttle_pct, int steering_pct) {
-//  int left_steering = abs(min(-steering_pct, steering_pct));
-//  int right_steering = abs(max(-steering_pct, steering_pct));
+void drive(int throttle_pct, int steering_pct, long height_steps) {
   int left_speed = throttle_pct + steering_pct;
   int right_speed = throttle_pct - steering_pct;
-//  if (throttle_pct < 0) {
-//    left_speed = -left_speed;
-//    right_speed = -right_speed;
-//  }
 
   IF_SERIAL Serial.println(
     "T: " + String(throttle_pct) +
@@ -163,39 +168,47 @@ void drive(int throttle_pct, int steering_pct) {
     "; R=" + String(right_speed));
 
   digitalWrite(STATUS_LED_PIN, HIGH);
-  #ifdef DRIVE_STEPPER
-  drive_stepper(left_speed, right_speed);
-  #endif
-  #ifdef DRIVE_PWM
-  drive_pwm(left_speed, right_speed);
-  #endif
+  drive_motors(left_speed, right_speed);
+  drive_height(height);
   digitalWrite(STATUS_LED_PIN, LOW);
 }
 
-#ifdef DRIVE_STEPPER
-void drive_stepper(int left_speed, int right_speed) {
-  // reverse right motor
-  right_speed = -right_speed;
-  left_stepper.setSpeed(map(left_speed, -THROTTLE_WEIGHT, THROTTLE_WEIGHT,
-    -MAX_SPEED_STEPS_PER_SEC, MAX_SPEED_STEPS_PER_SEC));
-  right_stepper.setSpeed(map(right_speed, -THROTTLE_WEIGHT, THROTTLE_WEIGHT,
-    -MAX_SPEED_STEPS_PER_SEC, MAX_SPEED_STEPS_PER_SEC));
-
+void drive_height(long height_steps) {
+  if (height_steps < H_STEPPER_HOME_THRESH) {
+    home_height();
+  } else if (abs(height_steps - height_stepper.currentPosition()) < H_STEPPER_FUZZ) {
+    height_stepper.disableOutputs();
+    return;
+  } else {
+    height_stepper.moveTo(height_steps);
+  }
+  height_stepper.enableOutputs();
   long start = millis();
   bool done = 1;
-  while (millis() < start + DRIVE_MS && millis() > start - 1) {
-    if (left_stepper.runSpeed()) done = 0;
-    if (right_stepper.runSpeed()) done = 0;
+  int dir = height_stepper.distanceToGo() / abs(height_stepper.distanceToGo());
+  height_stepper.setSpeed(dir * MAX_SPEED_STEPS_PER_SEC);
+  while (millis() < start + DRIVE_MS && millis() > start - 1 && !arm_at_home() && height_stepper.runSpeed()) {
+    // let the stepper run
   }
 }
 
-void stop_motors() {
-  left_stepper.setSpeed(0);
-  right_stepper.setSpeed(0);
+void home_height() {
+  if (!arm_at_home()) {
+    height_stepper.move(-H_STEPPER_FUZZ);
+  } else {
+    height_stepper.setCurrentPosition(0);
+    height_stepper.disableOutputs();
+  }
 }
-#endif
 
-#ifdef DRIVE_PWM
+bool arm_at_home() {
+  if (digitalRead(H_STEPPER_HOME_SW_PIN) == HIGH) {
+    height_stepper.setCurrentPosition(0);
+    return true;
+  }
+  return false;
+}
+
 void stop_motors() {
   digitalWrite(L_FORWARD_PIN, HIGH);
   digitalWrite(L_REVERSE_PIN, HIGH);
@@ -205,25 +218,12 @@ void stop_motors() {
   digitalWrite(L_REVERSE_SPEED_PIN, LOW);
   digitalWrite(R_FORWARD_SPEED_PIN, LOW);
   digitalWrite(R_REVERSE_SPEED_PIN, LOW);
+
+  height_stepper.setSpeed(0);
+  height_stepper.disableOutputs();
 }
 
-void drive_pwm(int left_speed, int right_speed) {
-//  if (left_speed > 0) {
-//    digitalWrite(L_FORWARD_PIN, HIGH);
-//    digitalWrite(L_REVERSE_PIN, LOW);
-//  } else {
-//    digitalWrite(L_FORWARD_PIN, LOW);
-//    digitalWrite(L_REVERSE_PIN, HIGH);
-//  }
-//
-//  if (right_speed > 0) {
-//    digitalWrite(R_FORWARD_PIN, HIGH);
-//    digitalWrite(R_REVERSE_PIN, LOW);
-//  } else {
-//    digitalWrite(R_FORWARD_PIN, LOW);
-//    digitalWrite(R_REVERSE_PIN, HIGH);
-//  }
-
+void drive_motors(int left_speed, int right_speed) {
   analogWrite(
     (left_speed < 0 ? L_REVERSE_SPEED_PIN : L_FORWARD_SPEED_PIN),
     constrain(map(abs(left_speed), 0, THROTTLE_WEIGHT, 0, PWM_MAX), 0, PWM_MAX));
@@ -236,6 +236,4 @@ void drive_pwm(int left_speed, int right_speed) {
   digitalWrite(
     (right_speed >= 0 ? R_REVERSE_SPEED_PIN : R_FORWARD_SPEED_PIN),
     LOW);    
-  delay(DRIVE_MS);
 }
-#endif
